9d76217ed4513b122f71989b48995954340ddb78
Nigel Khasiani <nigelkhasiani@gmail.com>
2025-06-19 10:29:41 +0300

    Added a card to contain the security stats graph

---
+++ src/src/components/layout/cards/SecurityChartCard.jsx
@@ -0,0 +1,11 @@
+function SecurityChartCard() {
+  return (
+    <div className="chart-card">
+      <h2>Chart Title</h2>
+      <div className="chart-container" id="security-chart">
+        {/* Chart component goes here */}
+      </div>
+    </div>
+  );
+}
+export default SecurityChartCard;
